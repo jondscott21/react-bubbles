be3e14b487be9abdc41885796c5281ddcbff235c
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-23 10:52:47 -0700

    final push, have some potion.js data displaying

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Pack } from "@potion/layout";
-import { Svg, Circle } from "@potion/element";
+import { Svg, Circle, Arc } from "@potion/element";
+import { Pie } from '@potion/layout';
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
+  const [pieData, setPieData] = useState([]);
   useEffect(() => {
     const generateBubbleData = colors.map((_, i) => ({
       value: Math.floor(Math.random() * (colors.length * 2)) + 1,
       key: `${i + 1}`
     }));
     setBubbleData(generateBubbleData);
+  }, [colors]);
+
+  useEffect(() => {
+    const generatePieData = colors.map((_, i) => ({
+      value: 1,
+      key: `${i + 1}`
+    }));
+    setPieData(generatePieData);
   }, [colors]);
 
   return (
@@ -45,6 +55,32 @@ const Bubbles = ({ colors }) => {
               .filter(v => v)
           }
         </Pack>
+      </Svg>
+      <Svg width={400} height={400}>
+        <Pie
+          data={pieData}
+          value={datum => datum.value}
+          id={datum => datum.id}
+          sort={(a, b) => a.id - b.id}
+          nodeEnter={d => ({ ...d, startAngle: d.endAngle })}
+          animate
+        >{nodes => nodes.map(({ startAngle, endAngle, key }, i) => {
+          if(i < colors.length) {
+            return (
+              <Arc
+                key={key}
+                innerRadius={0}
+                outerRadius={100}
+                startAngle={startAngle}
+                endAngle={endAngle}
+                fill={colors[i].code.hex}
+                stroke="white"
+                strokeWidth={3}
+              />
+            )
+          }
+          return null;
+        })}</Pie>
       </Svg>
     </div>
   );
